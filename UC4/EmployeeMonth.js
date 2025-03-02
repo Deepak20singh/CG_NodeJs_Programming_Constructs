b698f1f34926e9bd2db938867b35ca918735226a
Deepak20singh <137925992+Deepak20singh@users.noreply.github.com>
2025-03-03 00:07:02 +0530

    [Deepak] Add. Code of UC4 wages number of working days is given

---
+++ UC4/EmployeeMonth.js
@@ -0,0 +1,27 @@
+const num=2;
+let emphr=0;
+var part=1;
+var full=2;
+
+var parthr=4;
+var fullhr=8;
+var perhr=20;
+function getWork(empcheck){
+    switch(empcheck){
+        case part:
+            return parthr;
+          
+        case full:
+            return fullhr;
+     
+        default:
+          return 0;
+        
+    }}
+
+for(let day=0;day<num;day++){
+    let empcheck=Math.floor(Math.random()*10)%3;
+    emphr+=getWork(empcheck);
+}
+let empWage=emphr*perhr;
+console.log("Total Hrs :- "+emphr+" Emp Wages :- "+empWage);
