8f81a94ee06f6741bfec0f9eefe773f95dbbadd5
Deepak20singh <137925992+Deepak20singh@users.noreply.github.com>
2025-03-03 00:09:19 +0530

    [Deepak] Add. Code of UC3 using switch cases to find daily working hours

---
+++ UC2/EmployeeWages.js
@@ -0,0 +1,23 @@
+const part=1;
+const full=2;
+
+const parthr=4;
+const fullhr=8;
+const perhr=20;
+
+let emphr=0;
+empcheck=Math.floor(Math.random()*10)%3;
+switch(empcheck){
+    case part:
+        emphr=parthr;
+        break;
+    case full:
+        emphr=fullhr;
+    break;
+    default:
+        emphr=0;
+        break;
+}
+
+let empwage=emphr*perhr;
+console.log("Emp Wage :- "+empwage);
