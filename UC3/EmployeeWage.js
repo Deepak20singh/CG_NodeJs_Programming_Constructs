099f2a46c7e9216e6d6976a2894fbd413473571a
Deepak20singh <137925992+Deepak20singh@users.noreply.github.com>
2025-03-03 00:08:37 +0530

    [Deepak] Add. Code of UC3 using function to find daily working hours

---
+++ UC3/EmployeeWage.js
@@ -0,0 +1,25 @@
+var part=1;
+var full=2;
+
+var parthr=4;
+var fullhr=8;
+var perhr=20;
+
+let emphr=0;
+empcheck=Math.floor(Math.random()*10)%3;
+function getWork(empcheck){
+switch(empcheck){
+    case part:
+        return parthr;
+      
+    case full:
+        return fullhr;
+ 
+    default:
+      return 0;
+    
+}}
+
+emphr=getWork(empcheck);
+let empwage=emphr*perhr;
+console.log("Emp Wage :- "+empwage);
