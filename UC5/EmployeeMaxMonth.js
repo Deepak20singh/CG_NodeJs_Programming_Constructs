b8034b0b6a368402e9c18e3fd0e70ae5fd657d53
Deepak20singh <137925992+Deepak20singh@users.noreply.github.com>
2025-03-03 00:05:04 +0530

    [Deepak] Add. Code of UC5 wages with maximum days in month

---
+++ UC5/EmployeeMaxMonth.js
@@ -0,0 +1,32 @@
+const maxHrMonth=100;
+const maxDays=10;
+let totalempHr=0;
+const perhr=20;
+var part=1;
+var full=2;
+
+var parthr=4;
+var fullhr=8;
+
+let totalWorkDay=0;
+function getWork(empcheck){
+    switch(empcheck){
+        case part:
+            return parthr;
+          
+        case full:
+            return fullhr;
+     
+        default:
+          return 0;
+        
+    }}
+
+while(totalempHr<=maxHrMonth&&totalWorkDay<maxDays){
+    totalWorkDay++;
+    let empcheck=Math.floor(Math.random()*10)%3;
+    totalempHr+=getWork(empcheck);
+}
+
+let empWage=totalempHr*perhr;
+console.log("Total Days :- "+totalWorkDay+" Total Hrs :- "+totalempHr+" Emp Wage :- "+empWage);
